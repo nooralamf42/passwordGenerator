b8ecbe1cd10467793a5da63013b30fcdf46915f5
nooralamf42 <nooralamf42@gmail.com>
2023-09-02 20:36:05 +0530

    completed js part

--- app.js
+++ app.js
@@ -2,12 +2,54 @@ let passLength = document.getElementById("passLength");
 let range = document.getElementById("range");
 let passwordRange = range.value;
 let checkboxes = document.querySelectorAll("[type='checkbox']");
+let passwordDisplay = document.getElementById("passDisplay");
+let clipboardBtn = document.getElementById("copyBtn");
+let copyText = document.getElementById("copyText");
+let generateBtn = document.getElementById("generate");
+
+///// on rendering
 let password = "";
+passwordDisplay.value = "";
 
 //// changing range number on moving range slider
 range.onchange = ()=>{
     passLength.innerText = range.value;
+    if(range.value>5 && range.value<16){
+        if(range.value<10){
+            range.classList.remove(range.className.split(" ")[Array.from(range.className.split(" ")).length - 1]);
+            range.classList.add("accent-red-300")
+        }
+        else{
+            range.classList.remove(range.className.split(" ")[Array.from(range.className.split(" ")).length - 1]);
+            range.classList.add("accent-green-300")
+        }
+    }
+    else if(range.value>15){
+        range.classList.remove(range.className.split(" ")[Array.from(range.className.split(" ")).length - 1]);
+        range.classList.add("accent-green-500")
+    }
+    else{
+        range.classList.remove(range.className.split(" ")[Array.from(range.className.split(" ")).length - 1])
+        range.classList.add("accent-red-500")
+    }
     passwordRange = range.value;
+}
+
+//// function to copy to clipboard
+
+clipboardBtn.addEventListener('mouseover', ()=>{
+    if(password !== "")
+        clipboardBtn.classList.remove("hover:cursor-not-allowed");
+})
+
+function copyToClipboard(){
+    if(password !== ""){
+        navigator.clipboard.writeText(password);
+        copyText.innerText = "Copied";
+        setTimeout(()=>{
+            copyText.innerText = "Copy";
+        },1000)
+    }
 }
 
 //// functions to genrate random numbers and chars etc
@@ -40,27 +82,92 @@ let randomObject = {
     specialChar : randomSymb
 }
 
-//// function to check which checkboxes are checked
+let randomArray = []
 
-// function check(event){
-//     if(event.currentTarget.checked==true){
-//         password += randomObject[event.currentTarget.id]()
-//         console.log(password)
-//     }
-// }
+//// shuffle password function
+
+function shufflePassword(pass){
+    for(let i=0; i<pass.length; i++){
+        let j = Math.floor(Math.random()*(i+1));
+        [pass[i], pass[j]] = [pass[j], pass[i]];
+    }
+    let shuffle="";
+    pass.forEach(lettor=>{
+        shuffle+=lettor;
+    })
+    password = shuffle;
+    return shuffle;
+}
+
+//// create password function
+
+function createPassword(){
+    if(passwordRange<4){
+        passwordRange = 4;
+    }
+    for(let i=password.length; i<parseInt(passwordRange);i++){
+        password += randomObject[randomArray[randomNum(0,randomArray.length)]]()
+    }
+    passwordDisplay.value = shufflePassword(Array.from(password));
+}
+
+
+//// function to check which checkboxes are checked and create password
 
 function checkCheckBoxes (){
     password = ""
+    randomArray = []
     checkboxes.forEach(box => {
      if(box.checked){
         password += randomObject[box.id]()
+        randomArray.push(box.id)
      }
     })
-    console.log(password)
+
+    if(range.value<4){
+        passLength.innerText = 4;
+        range.value = 4;
+    }
+
+    if(password === ""){
+        checkboxes[0].checked = true;
+        randomArray.push("upper");
+    }
+    createPassword()
 }
 
-checkboxes.forEach(checkbox=>{
-    checkbox.addEventListener("click", checkCheckBoxes)
+//// to change color of password strength light
+
+
+function changeBtnColor(count){
+    if(count===2){
+        generateBtn.classList.remove(generateBtn.className.split(" ")[generateBtn.className.split(" ").length - 1])
+        generateBtn.classList.add("bg-green-200");
+    }
+    else if(count===3){
+        generateBtn.classList.remove(generateBtn.className.split(" ")[generateBtn.className.split(" ").length - 1])
+        generateBtn.classList.add("bg-green-400");
+    }
+    else if(count===4){
+        generateBtn.classList.remove(generateBtn.className.split(" ")[generateBtn.className.split(" ").length - 1])
+        generateBtn.classList.add("bg-green-600");
+    }
+    else{
+        generateBtn.classList.remove(generateBtn.className.split(" ")[generateBtn.className.split(" ").length - 1])
+        generateBtn.classList.add("bg-red-200");
+    }
+}
+let checkBoxCount = 0;
+checkboxes.forEach(box=>{
+    box.addEventListener("click", ()=>{
+        if(box.checked){
+            checkBoxCount += 1;
+        }
+        else{
+            checkBoxCount -= 1;
+        }
+        changeBtnColor(checkBoxCount)
+    })
 })
 
 
